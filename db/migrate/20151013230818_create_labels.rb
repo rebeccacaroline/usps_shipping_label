29a988dba411093ed6f7da86021ec6bf112b8a7d
rebeccacaroline <rebeccaholzschuh@gmail.com>
2015-10-13 16:21:32 -0700

    initial commit

---
+++ db/migrate/20151013230818_create_labels.rb
@@ -0,0 +1,10 @@
+class CreateLabels < ActiveRecord::Migration
+  def change
+    create_table :labels do |t|
+      t.text :sender
+      t.text :recipient
+      t.float :weight
+      t.text :dimensions
+    end
+  end
+end
