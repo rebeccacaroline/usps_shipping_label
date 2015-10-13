ec5cc8d5992c3fbeea7a9f8432d38a84f2a0bad2
rebeccacaroline <rebeccaholzschuh@gmail.com>
2015-10-13 16:38:02 -0700

    start on creation of label

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,2 +1,14 @@
 class LabelsController < ApplicationController
+
+  def index
+  end
+
+  def create
+    permitted = params.require(:label).
+                permit(:sender, :recipient, :weight, :dimensions, :customs_info)
+    label = Label.build(permitted)
+    unless label.save!
+      flash[:error] = "There was something wrong with the label."
+    end
+  end
 end
--- app/models/label.rb
+++ app/models/label.rb
@@ -1,2 +1,9 @@
 class Label < ActiveRecord::Base
+
+  serialize :sender, Hash
+  serialize :recipient, Hash
+  serialize :dimensions, Hash
+  serialize :customs_info, Hash
+
+
 end
